4c24562feecf81cc8a9a4a9a34a39bca16d3e70b
Tyler <dreamingimlatios@gmail.com>
2025-09-26 05:53:16 -0500

    Fix indexing issues (#36)
    * Fix glyph classification in shader
    
    * Fix shader indexing issues

--- Shaders/TmdSimpleUnlitFunctions.hlsl
+++ Shaders/TmdSimpleUnlitFunctions.hlsl
@@ -63,12 +63,12 @@ void DoSimpleUnlitFrag_float(float4 vertexColor, float4 uvAandB, float4 atlasInd
 		if (isSdf16)
 		{
 			UnityTexture2DArray sdf = GetSdf16TextureArray();
-			signedDistance = sdf.Sample(sampler_LinearClamp, uvA).a;
+			signedDistance = sdf.Sample(sampler_LinearClamp, uvA).r;
 		}
 		else
 		{
 			UnityTexture2DArray sdf = GetSdf8TextureArray();
-			signedDistance = sdf.Sample(sampler_LinearClamp, uvA).a;
+			signedDistance = sdf.Sample(sampler_LinearClamp, uvA).r;
 		}
 
 		float signedDistanceToEdge = (signedDistance - 0.5) * signedDistanceRatio; // Signed distance to edge, in Texture space
@@ -79,5 +79,57 @@ void DoSimpleUnlitFrag_float(float4 vertexColor, float4 uvAandB, float4 atlasInd
 		return;
 	}
 }
+
+void DoSimpleUnlitFragDebug_float(float4 vertexColor, float4 uvAandB, float4 atlasIndexScaleIsSdf16IsBitmap, out float3 finalColor, out float finalAlpha)
+{
+	float3 uvA = float3(uvAandB.xy, atlasIndexScaleIsSdf16IsBitmap.x);
+	float2 uvB = uvAandB.zw;
+	float scale = atlasIndexScaleIsSdf16IsBitmap.y;
+	bool isSdf16 = atlasIndexScaleIsSdf16IsBitmap.z;
+	bool isBitmap = atlasIndexScaleIsSdf16IsBitmap.w;
+
+	if (isBitmap)
+	{
+		UnityTexture2DArray bitmap = GetBitmapTextureArray();
+		float4 bitmapColor = bitmap.Sample(sampler_LinearClamp, uvA);
+		bitmapColor *= vertexColor;
+		finalColor = bitmapColor.xyz;
+		finalAlpha = bitmapColor.w;
+		return;
+	}
+	else
+	{
+		float screenSpaceRatio = rsqrt(abs(ddx(uvA.x) * ddy(uvA.y) - ddy(uvA.x) * ddx(uvA.y))) * GetGlyphTexelSize();
+
+		// For faking bold in this simple shader, we use the constant normal weight of 0 and bold weight of 0.75.
+		// After selecting the correct weight, this value is divided by 4, added to the outline width, and then divided by 2.
+		// Since we don't have outlines, we jump straight to the conclusion.
+		float isoPerimeter = scale >= 0.0 ? 0.0 : (0.75 / 8.0);
+
+		// The signed distance ratio is the padding value + 1.
+		// Todo: Need to pack the sampling point size enumeration into glyphEntryID.
+		float signedDistanceRatio = 11.0;
+
+		float signedDistance;
+		if (isSdf16)
+		{
+			UnityTexture2DArray sdf = GetSdf16TextureArray();
+			signedDistance = sdf.Sample(sampler_LinearClamp, uvA).r;
+		}
+		else
+		{
+			UnityTexture2DArray sdf = GetSdf8TextureArray();
+			signedDistance = sdf.Sample(sampler_LinearClamp, uvA).r;
+		}
+
+		float signedDistanceToEdge = (signedDistance - 0.5) * signedDistanceRatio; // Signed distance to edge, in Texture space
+		float pixelCoverage = saturate(signedDistanceToEdge * 2.0 * screenSpaceRatio + 0.5 + isoPerimeter * signedDistanceRatio * screenSpaceRatio);	// Screen pixel coverage (alpha)
+
+		finalColor = vertexColor.xyz; //float3(signedDistance, signedDistanceToEdge, pixelCoverage);
+		finalAlpha = 0.0; //vertexColor.w * pixelCoverage;
+		return;
+	}
+}
+
 
 #endif
